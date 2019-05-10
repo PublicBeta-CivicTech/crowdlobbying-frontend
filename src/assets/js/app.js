530c8ea2ab032ec611801aa2f3e7795b34c78b3c
Rene Keller <rek@pixelherz.com>
2019-05-10 16:27:13 +0200

    feat(officials): add canton-chooser

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,7 +2,11 @@ window.$ = window.jQuery = require('jquery')
 window.slick = require('slick-carousel')
 
 import 'what-input'
+import { MDCSelect } from '@material/select'
 import appMenu from './modules/menu'
+
+// MDC: Initialize material design components
+const select = new MDCSelect(document.querySelector('.mdc-select'))
 
 // App namespace
 window.app = {
