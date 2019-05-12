a48e73c939bc7d71bfc22411ca2ed0236c7730fc
Rene Keller <rek@pixelherz.com>
2019-05-12 10:07:20 +0200

    chore(version): set version, include in template

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -1,39 +1,45 @@
-const gulp = require("gulp");
-const config = require("../config").templates;
-const pug = require("gulp-pug");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
-const gutil = require("gulp-util");
-const htmlmin = require("gulp-htmlmin");
-const size = require("gulp-size");
-const cache = require("gulp-cache");
-const options = require("minimist")(process.argv.slice(2));
+const gulp = require('gulp')
+const config = require('../config').templates
+const pug = require('gulp-pug')
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+const gutil = require('gulp-util')
+const htmlmin = require('gulp-htmlmin')
+const size = require('gulp-size')
+const cache = require('gulp-cache')
+const data = require('gulp-data')
+const options = require('minimist')(process.argv.slice(2))
 
 const filters = {
   code(html, options) {
-    const lang = options.lang || "html";
+    const lang = options.lang || 'html'
     const preview =
-      lang === "html" ? `<div class="hf-preview">${html}</div>` : "";
+      lang === 'html' ? `<div class="hf-preview">${html}</div>` : ''
     return `
       <div class="hf-exemple">
         ${preview}
         <pre class="hf-code"><code class="js-highlight ${lang}">${html}</code></pre>
-      </div>`;
+      </div>`
   },
-};
+}
 
-gulp.task("templates", () => {
+gulp.task('templates', () => {
   return gulp
     .src(config.page_src)
     .pipe(
+      data(file => {
+        return { require: require }
+      })
+    )
+    .pipe(
       plumber({
-        errorHandler: notify.onError("PUG Error: <%= error.message %>"),
+        errorHandler: notify.onError('PUG Error: <%= error.message %>'),
       })
     )
     .pipe(cache(pug({ pretty: true, filters })))
     .pipe(
       options.production ? htmlmin({ collapseWhitespace: true }) : gutil.noop()
     )
-    .pipe(size({ title: "template" }))
-    .pipe(gulp.dest(config.dist));
-});
+    .pipe(size({ title: 'template' }))
+    .pipe(gulp.dest(config.dist))
+})
